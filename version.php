7b31454e0cc4e971c89ca53afc0c76e08d329dd2
shino <shino@kodamari.com>
2020-04-07 20:18:25 +0900

    Modify version.php can be installed on Moodle3.5

--- version.php
+++ version.php
@@ -26,5 +26,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2020040400;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2019111200;        // Requires this Moodle version
+$plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->component = 'block_teams_sync'; // Full name of the plugin (used for diagnostics)
